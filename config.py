625eccedbb12ecfe453ee360b63459c8cff9e2f0
AllSystem88 <deependray228@gmail.com>
2025-05-11 22:15:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(getenv("OWNER_ID", 1265397721))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-
+UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Kingdk88/RoyalQueen",
 )
